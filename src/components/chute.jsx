9495635f0afa9433cf7356415bad60e37b2de852
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-05-15 16:18:59 -0300

    Função chutar atualizada

--- src/components/chute.jsx
+++ src/components/chute.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 
 export default function Chute({palavra, setResultado, resultado, inGame}){
-    let [chute, letChute] = useState('');
+    let [chute, setChute] = useState('');
 
     return (
         <div  className="containerChute" >
             <h1>Já sabe a palavra?</h1>
-            <input type="text" onChange={(event) => letChute(event.target.value)}/>
+            <input type="text" value={chute} onChange={(event) => setChute(event.target.value)}/>
             <button 
-                onClick={()=> verificarChute(chute, palavra, setResultado)}
+                onClick={()=> verificarChute(chute, palavra, setResultado, setChute)}
                 disabled={( (!inGame) || (resultado !== '' || chute === '')) ? true : false}
             >
                 Chutar
@@ -17,10 +17,11 @@ export default function Chute({palavra, setResultado, resultado, inGame}){
         </div>
     );
 }
-function verificarChute(chute, palavra, setResultado){
+function verificarChute(chute, palavra, setResultado, setChute){
     if(chute === palavra){
-        setResultado('venceu');
+        setResultado('ganhou');
     }else{
         setResultado('perdeu');
     }
+    setChute('');
 }
